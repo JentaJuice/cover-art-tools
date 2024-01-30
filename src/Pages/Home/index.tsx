ee644bb7e38cc0bd1d1833bc31a11445efc42aca
Suphakit P <me@suphakit.net>
2024-01-30 07:42:19 +0700

    feat<index>: added index frame

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,3 +1,33 @@
+import { useState } from "react";
+import { Navigation } from "../../Components/Navigation";
+import { Cover } from "./CoverCrop";
+import { Resolution } from "./ResolutionCalculator";
+
 export function HomePage() {
-  return <></>;
+  const [page, setPage] = useState<string>();
+
+  const PageLoad = () => {
+    let returnVar;
+
+    if (page == "cover") {
+      returnVar = <Cover />;
+    } else {
+      returnVar = <Resolution />;
+    }
+
+    return returnVar;
+  };
+
+  return (
+    <>
+      <div className="w-full h-screen flex md:flex-row flex-col">
+        <div className="w-full md:w-[30%]">
+          <Navigation page={setPage} />
+        </div>
+        <div className="w-full md:w-full">
+          <PageLoad />
+        </div>
+      </div>
+    </>
+  );
 }
